c70bfd464931194779b29340d2616088e580ebc1
enriccio104 <142581742+enriccio104@users.noreply.github.com>
2024-08-05 17:44:17 +0200

    alecosta

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: 'la lazzarinihh', points: 4, image: 'rosso.png' },
         { name: 'sara la rue', points:3, image: 'verde.png' },
         { name: 'greg', points:3, image: 'rosso.png' },
+        { name: 'ale lacoste', points: 1, image: 'verde.png' },
         { name: 'pasqua', points: 1, image: 'rosso.png' },
         
     ];
