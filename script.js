f8f7fcf76dd1dc2b18d56b6540d595e088b1471e
enriccio104 <142581742+enriccio104@users.noreply.github.com>
2024-08-07 10:04:32 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const leaderboard = [
         { name: 'markury singols', points: 28, image: 'verde.png' },
-        { name: 'iphone rosso', points: 15, image: 'verde.png' },
-        { name: 'polettame xxx', points: 13, image: 'verde.png' },
+        { name: 'iphone rosso', points: 18, image: 'verde.png' },
+        { name: 'polettame xxx', points: 15, image: 'verde.png' },
+        { name: 'ovetto kinder', points: 14, image: 'verde.png' },
         { name: 'mela melinda', points: 13, image: 'rosso.png' },
-        { name: 'ovetto kinder', points: 12, image: 'rosso.png' },
         { name: 'la lazzarinihh', points: 7, image: 'verde.png' },
-        { name: 'diabete man', points: 4, image: 'rosso.png' },
+        { name: 'diabete man', points: 5, image: 'verde.png' },
         { name: 'sara la rue', points:3, image: 'rosso.png' },
         { name: 'greg', points:3, image: 'rosso.png' },
-        { name: 'ale lacoste', points: 1, image: 'verde.png' },
+        { name: 'maron', points: 1, image: 'verde.png' },
+        { name: 'ale lacoste', points: 1, image: 'rosso.png' },
         { name: 'pasqua', points: 1, image: 'rosso.png' },
-        
+        { name: 'jenny panytANO', points: 0, image: 'rosso.png' },
     ];
 
     const ul = document.getElementById('leaderboard');
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
    
     leaderboard.forEach((item, index) => {
         const li = document.createElement('li');
-        li.style.animationDelay = `${index * 0.3}s`; // Ritardo progressivo
+        li.style.animationDelay = `${index * 0.2}s`; // Ritardo progressivo
         li.innerHTML = `
             <div class="name"><strong>${index + 1}. ${item.name}</strong></div>
             <img src="${item.image}" alt="${item.name}" class="image">
